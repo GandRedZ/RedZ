a64e5274b6ef02790bd377cad82631751dce5cbc
Deurrr <josueruedabb@gmail.com>
2025-07-20 14:46:50 -0600

    knex configuration_migrations in development

---
+++ services/service_name/knexfile.ts
@@ -0,0 +1,47 @@
+import type { Knex } from "knex";
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST || "localhost",
+      port: parseInt(process.env.DB_PORT) || 5432,
+      user: process.env.DB_USER || "redz_user",
+      password: process.env.DB_PASSWORD || "redz_password",
+      database: process.env.DB_DATABASE || "redz_db",
+    },
+    migrations: {
+      directory: "./migrations",
+      extension: "ts",
+    },
+  },
+  test: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST || "localhost",
+      port: parseInt(process.env.DB_PORT) || 5432,
+      user: process.env.DB_USER || "redz_user",
+      password: process.env.DB_PASSWORD || "redz_password",
+      database: process.env.DB_DATABASE || "redz_db_test",
+    },
+    migrations: {
+      directory: "./migrations",
+      extension: "ts",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
+    migrations: {
+      directory: "./migrations",
+      extension: "ts",
+    },
+  },
+};
+export default config;
